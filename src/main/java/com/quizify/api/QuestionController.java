acb6d582c6dc3627f34eb3b5d8d9ac67807fa68f
KeitaroBlaster <rskp95@yahoo.fr>
2024-03-25 19:49:17 +0100

    Maj import @requestBody

--- src/main/java/com/quizify/api/QuestionController.java
+++ src/main/java/com/quizify/api/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,7 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.quizify.model.Question;
 import com.quizify.services.ServiceQuestion;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 
